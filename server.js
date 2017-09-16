98df3c0159fbaf9b9379978edb99b35a4337fd3f
Slavik3011 <Slavik3011@yandex.ru>
2017-09-16 13:50:36 +0300

    OAuth

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import bluebird from 'bluebird';
 import config from './config';
 import authRoute from './routes/auth';
 import errorHandler from './middlewares/errorHandler';
+import checkToken from './middlewares/checkToken';
 
 const app = express();
 
@@ -35,10 +36,9 @@ app.use(session({
 	secret: config.secret
 }));
 
-app.get('*', async (req, res) => {
-	res.end('Hello world');
-})
-
 app.use('/api', authRoute);
+app.get('/test', checkToken, (req, res) => {
+	res.json('test')
+})
 
 app.use(errorHandler)
